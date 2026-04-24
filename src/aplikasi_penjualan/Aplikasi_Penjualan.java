f601ad31bf352ec466e5c51527feee1cb7798ac4
hp <hp@DESKTOP-QFC1AM5>
2026-04-24 09:33:01 +0700

    first commit

---
+++ src/aplikasi_penjualan/Aplikasi_Penjualan.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi_penjualan;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.sql.*;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.KeyEvent;
+import koneksi.koneksi;
+
+public class Aplikasi_Penjualan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
